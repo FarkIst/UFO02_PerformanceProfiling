139c76cd92486352f70157f73cff53c9dbcc6de9
FarkIst <FarkIst@github.com>
2019-11-10 14:20:25 +0100

    add optimized

--- src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
+++ src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -1,9 +1,16 @@
 package cphbusiness.ufo.letterfrequencies;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -17,23 +24,50 @@ import static java.util.stream.Collectors.toMap;
  * @author kasper
  */
 public class Main {
-
+    
+    private static String _fileContents;
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String fileName = "/Users/kasper/GITHUB/ufo/letterfrequencies/src/main/resources/FoundationSeries.txt";
-        Reader reader = new FileReader(fileName);
-        Map<Integer, Long> freq = new HashMap<>();
-        tallyChars(reader, freq);
-        print_tally(freq);
+        String fileName = "./src/main/resources/FoundationSeries.txt";
+        readToString(fileName);
+        Reader reader = new StringReader(_fileContents);
+       // Map<Integer, Long> freq = new HashMap<>();
+
+        tallyCharsOptimized();
+       // tallyChars(reader, freq);
+       // print_tally(freq);
     }
 
+    private static void readToString(String fileName) throws IOException{
+        _fileContents = new String(Files.readAllBytes(Paths.get(fileName)), StandardCharsets.UTF_8);
+    }
+    
+    private static void tallyCharsOptimized(){
+        HashMap<Integer, Integer> hash = new HashMap<>();
+
+            for (int i = 0; i < _fileContents.length(); i++) {
+                char c = _fileContents.charAt(i);
+                if (c != ' ') {
+                    // Increment existing value in HashMap.
+                    // ... Start with zero if no key exists.
+                    int value = hash.getOrDefault((int) c, 0);
+                    hash.put((int) c, value + 1);
+                }
+            }  
+            
+            for (int key : hash.keySet()) {
+            System.out.println((char) key + ": " + hash.get(key));
+        }
+    }
+    
     private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
         int b;
         while ((b = reader.read()) != -1) {
             try {
                 freq.put(b, freq.get(b) + 1);
             } catch (NullPointerException np) {
-                freq.put(b, 1L);
-            };
+                freq.put(b, 2L);                
+            }
         }
     }
 
